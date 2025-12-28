d1f814e3f4155dad0bafe0257cc711880d665a98
gs32alwin-dev <gs32alwin@gmail.com>
2025-12-28 23:12:03 +0530

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -70,4 +70,198 @@ export const INITIAL_MATERIALS: Material[] = [
     url: "https://en.wikipedia.org/wiki/Chemical_reaction",
     date: "2024-05-20"
   }
+];// UPDATE constants.tsx WITH THIS CODE:
+
+export const INITIAL_FOLDERS: Folder[] = [
+  {
+    "id": "seed-folder-1",
+    "subjectId": "math",
+    "name": "Chapter 1: Number Systems",
+    "createdAt": "2024-05-20"
+  },
+  {
+    "id": "seed-folder-2",
+    "subjectId": "science",
+    "name": "Unit 1: Chemical Reactions",
+    "createdAt": "2024-05-20"
+  },
+  {
+    "id": "fld-z023zsuyp",
+    "subjectId": "science",
+    "name": "Chapter 1 - Chemical Reaction and Equation",
+    "createdAt": "12/28/2025"
+  }
 ];
+
+export const INITIAL_MATERIALS: Material[] = [
+  {
+    "id": "mat-j98yxkqbc",
+    "subjectId": "science",
+    "folderId": "fld-z023zsuyp",
+    "title": "Audio",
+    "type": "doc",
+    "date": "12/28/2025",
+    "url": "blob:https://playful-vacherin-f0058b.netlify.app/c7fc95a2-7f24-44de-9dc3-bbff3e50e321"
+  },
+  {
+    "id": "mat-q6favg563",
+    "subjectId": "science",
+    "folderId": "fld-z023zsuyp",
+    "title": "Video",
+    "type": "video",
+    "date": "12/28/2025",
+    "url": "blob:https://playful-vacherin-f0058b.netlify.app/0bd527ba-6e38-4456-ba49-8bea69337b5f"
+  },
+  {
+    "id": "seed-mat-1",
+    "subjectId": "math",
+    "folderId": "seed-folder-1",
+    "title": "Introduction to Rational Numbers (Video)",
+    "type": "video",
+    "url": "https://www.w3schools.com/html/mov_bbb.mp4",
+    "date": "2024-05-20"
+  },
+  {
+    "id": "seed-mat-2",
+    "subjectId": "math",
+    "folderId": "seed-folder-1",
+    "title": "Number System Flowchart",
+    "type": "image",
+    "url": "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=1000",
+    "date": "2024-05-20"
+  },
+  {
+    "id": "seed-mat-3",
+    "subjectId": "science",
+    "folderId": "seed-folder-2",
+    "title": "Class Notes - Balancing Equations",
+    "type": "link",
+    "url": "https://en.wikipedia.org/wiki/Chemical_reaction",
+    "date": "2024-05-20"
+  }
+];// UPDATE constants.tsx WITH THIS CODE:
+
+export const INITIAL_FOLDERS: Folder[] = [
+  {
+    "id": "seed-folder-1",
+    "subjectId": "math",
+    "name": "Chapter 1: Number Systems",
+    "createdAt": "2024-05-20"
+  },
+  {
+    "id": "seed-folder-2",
+    "subjectId": "science",
+    "name": "Unit 1: Chemical Reactions",
+    "createdAt": "2024-05-20"
+  },
+  {
+    "id": "fld-z023zsuyp",
+    "subjectId": "science",
+    "name": "Chapter 1 - Chemical Reaction and Equation",
+    "createdAt": "12/28/2025"
+  }
+];
+
+export const INITIAL_MATERIALS: Material[] = [
+  {
+    "id": "mat-j98yxkqbc",
+    "subjectId": "science",
+    "folderId": "fld-z023zsuyp",
+    "title": "Audio",
+    "type": "doc",
+    "date": "12/28/2025",
+    "url": "blob:https://playful-vacherin-f0058b.netlify.app/c7fc95a2-7f24-44de-9dc3-bbff3e50e321"
+  },
+  {
+    "id": "mat-q6favg563",
+    "subjectId": "science",
+    "folderId": "fld-z023zsuyp",
+    "title": "Video",
+    "type": "video",
+    "date": "12/28/2025",
+    "url": "blob:https://playful-vacherin-f0058b.netlify.app/0bd527ba-6e38-4456-ba49-8bea69337b5f"
+  },
+  {
+    "id": "seed-mat-1",
+    "subjectId": "math",
+    "folderId": "seed-folder-1",
+    "title": "Introduction to Rational Numbers (Video)",
+    "type": "video",
+    "url": "https://www.w3schools.com/html/mov_bbb.mp4",
+    "date": "2024-05-20"
+  },
+  {
+    "id": "seed-mat-2",
+    "subjectId": "math",
+    "folderId": "seed-folder-1",
+    "title": "Number System Flowchart",
+    "type": "image",
+    "url": "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=1000",
+    "date": "2024-05-20"
+  },
+  {
+    "id": "seed-mat-3",
+    "subjectId": "science",
+    "folderId": "seed-folder-2",
+    "title": "Class Notes - Balancing Equations",
+    "type": "link",
+    "url": "https://en.wikipedia.org/wiki/Chemical_reaction",
+    "date": "2024-05-20"
+  }
+]; */
+
+export const INITIAL_FOLDERS: Folder[] = [
+  {
+    "id": "seed-folder-1",
+    "subjectId": "math",
+    "name": "Chapter 1: Number Systems",
+    "createdAt": "2024-05-20"
+  },
+  {
+    "id": "seed-folder-2",
+    "subjectId": "science",
+    "name": "Unit 1: Chemical Reactions",
+    "createdAt": "2024-05-20"
+  }
+];
+
+export const INITIAL_MATERIALS: Material[] = [
+  {
+    "id": "mat-n170tmhst",
+    "subjectId": "science",
+    "title": "image",
+    "type": "image",
+    "date": "12/28/2025",
+    "url": "blob:https://665w8l6d4ctp6mz3o80ksl29tkvt8rvf5tyya9xiyc7wrmoz8o-h845251650.scf.usercontent.goog/8d853a4e-7e0f-4c5b-acc5-d8d123b51a2d"
+  },
+  {
+    "id": "seed-mat-1",
+    "subjectId": "math",
+    "folderId": "seed-folder-1",
+    "title": "Introduction to Rational Numbers (Video)",
+    "type": "video",
+    "url": "https://www.w3schools.com/html/mov_bbb.mp4",
+    "date": "2024-05-20"
+  },
+  {
+    "id": "seed-mat-2",
+    "subjectId": "math",
+    "folderId": "seed-folder-1",
+    "title": "Number System Flowchart",
+    "type": "image",
+    "url": "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=1000",
+    "date": "2024-05-20"
+  },
+  {
+    "id": "seed-mat-3",
+    "subjectId": "science",
+    "folderId": "seed-folder-2",
+    "title": "Class Notes - Balancing Equations",
+    "type": "link",
+    "url": "https://en.wikipedia.org/wiki/Chemical_reaction",
+    "date": "2024-05-20"
+  }
+];
+Discard Local Edits
+
+
